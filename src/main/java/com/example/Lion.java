1566da8b0bfda42859517596b796a630decec0d7
Dmitriy Zhdanov <uzver1004@gmail.com>
2024-10-24 02:41:02 +0700

    git add test

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -3,30 +3,30 @@ package com.example;
 import java.util.List;
 
 public class Lion {
+    private boolean hasMane;
+    private Predator predator;
 
-    boolean hasMane;
+    public Lion(String sex, Predator predator) {
+        this.predator = predator;
 
-    public Lion(String sex) throws Exception {
         if ("Самец".equals(sex)) {
             hasMane = true;
         } else if ("Самка".equals(sex)) {
             hasMane = false;
         } else {
-            throw new Exception("Используйте допустимые значения пола животного - самей или самка");
+            throw new IllegalArgumentException("Используйте допустимые значения пола: Самец или Самка");
         }
-    }
-
-    Feline feline = new Feline();
-
-    public int getKittens() {
-        return feline.getKittens();
     }
 
     public boolean doesHaveMane() {
         return hasMane;
     }
 
+    public int getKittens() {
+        return ((Feline) predator).getKittens();
+    }
+
     public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
+        return predator.eatMeat();
     }
 }
